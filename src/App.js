de7bf9af0bbe8429e2caad1e237ed3470ab8f0a2
Учетная запись Обучение Александр <Sascha@iMac-Aliaksandr.local>
2022-08-11 22:34:28 +0300

    complit task 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div>
+// Hello React
+//     </div>
+//   );
+// }
+//
+// export default App;
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+const Person = {
+    name: 'Peter',
+    age: 24,
+    gender: 'male'
+    }
+    const routes =['home', 'about as', 'contact', 'news'];
+
+    return (
+        <div className='App'>
+        <Header routes={routes}/>
+            <main>
+        <h1>Manchester United</h1>
+            <Content person={Person}/>
+            </main>
+            <Footer/>
     </div>
-  );
+    )
 }
 
 export default App;
